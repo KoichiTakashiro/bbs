04005e8f4761bf91325b37d524946fdac3e20018
Koichi <cx.w0077@gmail.com>
2016-01-22 16:33:05 +0800

    dbconnect修正

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
     //$db = mysqli_connect('localhost','root','root','exam') or die(mysqli_connect_error());
-    $db = mysqli_connect('153.121.67.164','ktakashiro','v1ELbFn9','ktakashiro_db') or die(mysqli_connect_error());
+    $db = mysqli_connect('localhost','ktakashiro','v1ELbFn9','ktakashiro_db') or die(mysqli_connect_error());
 
     mysqli_set_charset($db,'utf8');
  ?>
